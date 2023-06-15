8ccbbf2d613ba24416848a1c5b6993835d683850
Jokaah <43284613+Jokahh@users.noreply.github.com>
2023-06-15 14:58:47 +0200

    Añadido y configurado "knplabs/knp-paginator-bundle"

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
