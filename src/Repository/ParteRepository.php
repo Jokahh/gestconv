6d27f03e8c8b4f282b7c919df67cd11ea55531f9
Jokaah <43284613+Jokahh@users.noreply.github.com>
2023-06-19 13:33:10 +0200

    Método findAllSancionablesPorEstudiante

--- src/Repository/ParteRepository.php
+++ src/Repository/ParteRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repository;
 
+use App\Entity\Estudiante;
 use App\Entity\Parte;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
@@ -19,6 +20,28 @@ class ParteRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Parte::class);
+    }
+
+    public function findAllSancionablesPorEstudiante(Estudiante $estudiante): array
+    {
+        $queryBuilder = $this->createQueryBuilder('parte');
+        $queryBuilder
+            ->where('parte.estudiante = :estudiante')
+            ->andWhere('parte.sancion IS NULL')
+            ->andWhere('parte.prescrito = false')
+            ->andWhere('parte.fechaAviso IS NOT NULL')
+            ->orderBy('parte.fechaSuceso')
+            ->setParameter('estudiante', $estudiante);
+        return $queryBuilder->getQuery()->getResult();
+    }
+
+    public function findOneById(int $id)
+    {
+        $queryBuilder = $this->createQueryBuilder('parte');
+        $queryBuilder
+            ->where('parte.id = :id')
+            ->setParameter('id', $id);
+        return $queryBuilder->getQuery()->getResult();
     }
 
     public function nuevo(): Parte
